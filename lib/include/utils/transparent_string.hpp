5ead493dc53af69df3d2980b481bb4f0eb69538c
92half99 <matthewbeshay@outlook.com>
2025-06-01 21:24:06 +1000

    Updated to main branch

--- lib/include/utils/transparent_string.hpp
+++ lib/include/utils/transparent_string.hpp
@@ -1,33 +1,34 @@
 #pragma once
 
-// C++ Standard Library
 #include <concepts>
-#include <functional>
 #include <string>
 #include <string_view>
+#include <functional>
 
-/// Heterogeneous hash functor for any string-like type convertible to std::string_view.
-/// Allows unordered_map to accept std::string_view, const char*, std::string, etc., without
-/// allocation.
+//----------------------------------------------------------------
+// TransparentStringHash with C++20 concepts
+//----------------------------------------------------------------
 struct TransparentStringHash {
     using is_transparent = void;
 
-    template <std::convertible_to<std::string_view> S>
-    std::size_t operator()(S const &s) const noexcept
-    {
-        // Normalize to string_view, then use std::hash<string_view>.
+    template<std::convertible_to<std::string_view> S>
+    std::size_t operator()(S const& s) const noexcept {
+        // We always normalize to a std::string_view, then hash.
         return std::hash<std::string_view>{}(std::string_view(s));
     }
 };
 
-/// Heterogeneous equality comparator for any string-like type convertible to std::string_view.
-/// Enables unordered_map to compare keys of different string-like types.
+//----------------------------------------------------------------
+// TransparentStringEq with C++20 concepts
+//----------------------------------------------------------------
 struct TransparentStringEq {
     using is_transparent = void;
 
-    template <std::convertible_to<std::string_view> A, std::convertible_to<std::string_view> B>
-    bool operator()(A const &a, B const &b) const noexcept
-    {
+    template<
+        std::convertible_to<std::string_view> A,
+        std::convertible_to<std::string_view> B
+    >
+    bool operator()(A const& a, B const& b) const noexcept {
         return std::string_view(a) == std::string_view(b);
     }
 };
